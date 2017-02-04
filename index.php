09da25e80a71885141273e4293fff91625dcdd96
takedajs <takedajs>
2017-02-04 18:29:05 +0900

    Guzzle動作確認用

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "a";
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new GuzzleHttp\Client();
+
+$res = $client->get('http://192.168.33.10/response.php');
+
+$body = $res->getBody();
+
+echo $body->getContents();
---
+++ response.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Guzzle動作確認成功";
